a94228142d86ce576fd931bc222044b4938e4c14
Thamotharan Chandran <thamotharan.c@softcrylic.co.in>
2023-11-27 15:36:34 +0530

    django_project_dev

--- farming/farmingapp/urls.py
+++ farming/farmingapp/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('', views.login),
     path('login', views.login, name= 'login'),
+    path('signup', views.signup, name= 'signup'),
     path('home', views.home, name= 'home'),
     path('seller', views.seller, name= 'seller'),
     path('logout', views.logout, name='logout')
